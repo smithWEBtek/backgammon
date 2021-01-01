80fcd7676f63ed29574402b90d45eba95c9a7e39
smithwebtek <brad@smithwebtek.com>
2020-12-31 23:34:26 -0500

    Add triangls

--- src/components/Stripe/Stripe.js
+++ src/components/Stripe/Stripe.js
@@ -7,7 +7,11 @@ class Stripe extends Component {
     const triangle = (
       <div className="triangle-container">
         <svg height="500" width="500">
-          <polygon points="150,60 100,400 200,400" className="triangle" />
+          <polygon points="150,60 200,400 100,400" className="triangle" />
+          <polygon points="250,60 200,400 300,400" className="triangle" />
+          <polygon points="350,60 300,400 400,400" className="triangle" />
+          <polygon points="450,60 400,400 500,400" className="triangle" />
+ 
         </svg>
       </div>
     );
