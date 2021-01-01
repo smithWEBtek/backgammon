c6cc4a794548903a0b9776a64e05c4c8376bf313
smithwebtek <brad@smithwebtek.com>
2021-01-01 14:00:04 -0500

    First quarter board

--- src/components/Stripe/Stripe.js
+++ src/components/Stripe/Stripe.js
@@ -6,12 +6,13 @@ class Stripe extends Component {
   render() {
     const triangle = (
       <div className="triangle-container">
-        <svg height="500" width="500">
-          <polygon points="150,60 200,400 100,400" className="triangle" />
-          <polygon points="250,60 200,400 300,400" className="triangle" />
-          <polygon points="350,60 300,400 400,400" className="triangle" />
-          <polygon points="450,60 400,400 500,400" className="triangle" />
- 
+        <svg height="500" width="1000">
+          <polygon id="1" points="75,190 100,400 50,400" className="triangle-black" />
+          <polygon id="2" points="125,190 100,400 150,400" className="triangle-white" />
+          <polygon id="3" points="175,190 150,400 200,400" className="triangle-black" />
+          <polygon id="4" points="225,190 200,400 250,400" className="triangle-white" />
+          <polygon id="5" points="275,190 250,400 300,400" className="triangle-black" />
+          <polygon id="6" points="325,190 300,400 350,400" className="triangle-white" />
         </svg>
       </div>
     );
