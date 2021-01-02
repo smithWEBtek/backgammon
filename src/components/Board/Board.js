1cbd04e741d7e66b1510733174994840b2b0d812
smithwebtek <brad@smithwebtek.com>
2021-01-02 10:11:39 -0500

    Start top row of board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './Board.css';
 import StripesBottomLeft from '../Stripes/StripesBottomLeft';
 import StripesBottomRight from '../Stripes/StripesBottomRight';
+import StripesUpperLeft from '../Stripes/StripesUpperLeft';
 import Bar from '../Bar/Bar';
 
 class Board extends Component {
   render () {
       return (
         <>
-          <h2>The Board</h2>
+          <h2>Backgammon</h2>
         <div className="board">
+          <StripesUpperLeft />  
           <StripesBottomLeft />  
           <Bar />
           <StripesBottomRight />
